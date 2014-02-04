5e8d6903fe8eb11950c08a1706febb10137cef3d
gnzlbg <gonzalobg88@gmail.com>
2014-02-04 13:41:09 +0100

    add range copy

---
+++ thrust/range/algorithms/copy.h
@@ -0,0 +1,52 @@
+///
+///  Copyright 2014 Gonzalo Brito Gadeschi
+///
+///  Licensed under the Apache License, Version 2.0 (the "License");
+///  you may not use this file except in compliance with the License.
+///  You may obtain a copy of the License at
+///
+///  http://www.apache.org/licenses/LICENSE-2.0
+///
+///  Unless required by applicable law or agreed to in writing, software
+///  distributed under the License is distributed on an "AS IS" BASIS,
+///  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+///  See the License for the specific language governing permissions and
+///  limitations under the License.
+
+/// \file \brief Implements the rang-based interface of the copy algorithm
+
+#pragma once
+
+#include <type_traits>
+#include <thrust/copy.h>
+#include <thrust/range/utilities.h>
+
+namespace thrust
+{
+
+/// \addtogroup algorithms
+
+///\addtogroup copying
+///  \ingroup algorithms
+/// \{
+
+/// \brief Copies the \t SinglePassRange \p range to the \t OutputIterator \p
+/// result
+template<typename SinglePassRange, typename OutputIterator>
+typename std::enable_if<!models::single_pass_range<OutputIterator>::value,
+                        OutputIterator>::type
+copy(SinglePassRange const& range, OutputIterator result)
+{ return copy(range.begin(), range.end(), result); }
+
+/// \brief Copies the \t SinglePassRange range to the \t OutputRange \p result.
+template<typename SinglePassRange, typename OutputRange>
+typename std::enable_if<models::single_pass_range<OutputRange>::value,
+                        OutputRange>::type
+copy(SinglePassRange const& range, OutputRange& result) {
+  copy(range.begin(), range.end(), result.begin());
+  return result;
+}
+
+/// \}  // end copying
+
+}  // namespace thrust
